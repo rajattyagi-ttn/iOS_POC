165d3d7ae1f51d41dacdf308824e07a0e94d45cd
rajattyagi-ttn <60641678+rajattyagi-ttn@users.noreply.github.com>
2020-08-12 14:17:43 +0530

    Played Music through Bluetooth

--- Bluetooth_POC/BluetoothTest/AppDelegate.swift
+++ Bluetooth_POC/BluetoothTest/AppDelegate.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            // Set the audio session category, mode, and options.
+            try audioSession.setCategory(.playAndRecord, options: .allowBluetooth)
+        } catch {
+            print("Failed to set audio session category.")
+        }
+        
+        // Other post-launch configuration.
         return true
     }
 
@@ -24,11 +33,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        if #available(iOS 13.0, *) {
-            return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-        } else {
-            // Fallback on earlier versions
-        }
+        
+        
+        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
--- Bluetooth_POC/BluetoothTest/ViewController.swift
+++ Bluetooth_POC/BluetoothTest/ViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 import CoreBluetooth
+import AVFoundation
 
 class ViewController: UIViewController , CBPeripheralDelegate, CBCentralManagerDelegate{
     private var centralManager: CBCentralManager!
     private var peripheral: CBPeripheral!
     var peripherals = Array<CBPeripheral>()
-    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         centralManager = CBCentralManager(delegate: self, queue: nil)
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "LP", ofType: "mp3")!))
+            createBackgroundAudioSession()
+        }
+        
+        catch{
+            print(error)
+        }
+        
     }
+    
+        private func createBackgroundAudioSession() {
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [.allowAirPlay, .defaultToSpeaker, .allowBluetooth, .allowBluetoothA2DP])
+                try AVAudioSession.sharedInstance().setActive(true)
+            } catch {
+                print(error)
+            }
+            
+            var outputVolumeObserve: NSKeyValueObservation? = AVAudioSession.sharedInstance().observe(\.outputVolume) { (audioSession, changes) in
+                print("New volume: \(changes.newValue)")
+            }
+        }
 
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
@@ -40,8 +63,8 @@ class ViewController: UIViewController , CBPeripheralDelegate, CBCentralManagerD
 
         }
         self.peripheral = peripheral
-               self.peripheral.delegate = self
-        if peripheral.name == "OnePlus Bullets Wireless Z" {
+        self.peripheral.delegate = self
+        if peripheral.name == "MI Band 2" {
             self.centralManager.connect(self.peripheral, options: nil)
             self.centralManager.stopScan()
         }
@@ -53,18 +76,49 @@ class ViewController: UIViewController , CBPeripheralDelegate, CBCentralManagerD
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         if peripheral == self.peripheral {
-            print("Connected to your \(String(describing: peripheral.name))")
-        
+            print("Connected to your device: \(String(describing: peripheral.name))")
+//            print("Connected to Service : \(String(describing: peripheral.services))")
+
         }
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        if let services = peripheral.services {
-            for service in services {
-                print("Service : \(service)")
+        print(error ?? peripheral.services)
+        peripheral.services?.forEach { service in
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        print(error ?? service.characteristics)
+        service.characteristics?.forEach { characteristic in
+            if characteristic.properties.contains(.read) {
+                peripheral.readValue(for: characteristic)
+            }
+            if characteristic.properties.contains(.notify) {
+                peripheral.setNotifyValue(true, for: characteristic)
             }
         }
     }
-
+    
+     
+//    private func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+//        print("Services:\(String(describing: peripheral.services)) and error\(String(describing: error))")
+//        if let services = peripheral.services {
+//            for service in services {
+//                print(service)
+//            }
+//        }
+//    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        audioPlayer.play()
+    }
+    @IBAction func pauseTapped(_ sender: Any) {
+        if audioPlayer.isPlaying {
+            audioPlayer.pause()
+        }
+    }
+    
 }
 
